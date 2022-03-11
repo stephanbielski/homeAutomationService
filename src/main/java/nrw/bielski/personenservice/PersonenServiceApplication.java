72aa534d8faa77485165311fd9fc4ba16eeef4b5
stephanbielski <stephan@bielski.nrw>
2022-03-11 02:05:30 +0100

    Initial commit

---
+++ src/main/java/nrw/bielski/personenservice/PersonenServiceApplication.java
@@ -0,0 +1,13 @@
+package nrw.bielski.personenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonenServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonenServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nrw/bielski/personenservice/PersonenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nrw.bielski.personenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
