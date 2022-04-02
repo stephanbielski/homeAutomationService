946d4f921e58c3ec1960dc3f280489f8b0e9c5cc
stephanbielski <stephan@bielski.nrw>
2022-04-02 09:58:33 +0200

    Error.java lombok builder removed

--- src/main/java/nrw/bielski/services/ErrorService.java
+++ src/main/java/nrw/bielski/services/ErrorService.java
@@ -25,38 +25,20 @@ public class ErrorService {
 
     private final HttpServletRequest request;
 
-    /**
-     * The constructor used for the injection of the fields (constructor injection). (Direct) field injection is not
-     * thread-safe here since this class is a Spring {@link Service} bean.
-     *
-     * @param request the {@link HttpServletRequest} object for providing meta information, never {@code null}
-     */
     @Autowired
     public ErrorService(final HttpServletRequest request) {
         this.request = request;
     }
 
-    /**
-     * Prepares the {@link Error} object with commonly used values.
-     *
-     * @param title         the error's title, must not be {@code null}
-     * @param detail        the error's detailed description, may be {@code null}
-     * @param invalidParams special field for parameter violations, may be {@code null}
-     *
-     * @return the final {@link Error}, never {@code null}
-     */
     public Error finalizeRfc7807Error(final String title, final String detail,
                                       @Valid final List<Error.InvalidParam> invalidParams) {
         final UUID errorId = UUID.randomUUID();
         log.warn("Problems in request. ID: {}", errorId);
 
-        Error error = new Error(URI.create(request.getRequestURI())
+        return new Error(URI.create(request.getRequestURI())
                 ,title
                 ,URI.create(request.getRequestURI())
                 ,detail
                 ,invalidParams);
-
-        return error;
     }
-
 }
